c6ca6e3734207b6c4a6c830123d95a1f1037281f
Axel Bizel <axel.bizel@gmail.com>
2019-11-20 22:51:35 +0100

    experience component

---
+++ src/Components/Experience.js
@@ -0,0 +1,143 @@
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Row,
+  Col,
+  Button
+} from "reactstrap";
+import WCSlogo from "../Images/WCS.jpg";
+import IDIX from "../Images/idix.png";
+import LaCroix from "../Images/lacroix.png";
+import Mind from "../Images/mind-logo.jpg";
+
+const Experience = () => {
+  return (
+    <div>
+        <h2>Expérience</h2>
+      <Row>
+        <Col sm="6">
+          <Card>
+            {/* <CardImg top width="100%" src={WCSlogo} alt="Card image cap" /> */}
+            <CardBody className="text-left">
+              <CardTitle>
+                <img src={WCSlogo} alt="Wild Code School Logo" width="100px" />
+                <h3>Développeur front-end</h3>
+              </CardTitle>
+              <CardSubtitle>
+                <h4>Wild Code School - 2019-2020</h4>
+              </CardSubtitle>
+              <CardText>
+                <p>
+                  <ul>
+                    <li>Projet 1 : site statique - tinyurl.com/lareligieuse</li>
+                    <li>
+                      Projet 2 : jeu en React.js - tinyurl.com/strangler-things
+                    </li>
+                    <li>Projet 3 : en cours</li>
+                    <li>Participation à deux Hackathons</li>
+                  </ul>
+                </p>
+              </CardText>
+              {/* 
+              <Button>Button</Button> */}
+            </CardBody>
+          </Card>
+        </Col>
+        <Col sm="6">
+          <Card>
+            <CardBody className="text-left">
+              <CardTitle>
+                <img src={IDIX} alt="Idix Logo" height="100px" />
+                <h3>Datajournaliste / Infographiste</h3>
+              </CardTitle>
+              <CardSubtitle>
+                <h4>Agence IDIX (ex-agence Idé) - 2015-2018</h4>
+              </CardSubtitle>
+              <CardText>
+                <p>
+                  <ul>
+                    <li>
+                      Conception et réalisation de datavisualisations pour la
+                      presse
+                    </li>
+                    <li>
+                      Traitement, analyse et nettoyage de bases de données
+                    </li>
+                    <li>
+                      Pilotage de projets web (dispositifs électoraux, etc…)
+                    </li>
+                  </ul>
+                </p>
+              </CardText>
+              {/* <Button>Button</Button> */}
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col sm="6">
+          <Card>
+            {/* <CardImg top width="100%" src={WCSlogo} alt="Card image cap" /> */}
+            <CardBody className="text-left">
+              <CardTitle>
+                <img src={Mind} alt="Mind Logo" height="100px" />
+                <h3>Journaliste spécialisé économie numérique</h3>
+              </CardTitle>
+              <CardSubtitle>
+                <h4>Mind Média (ex-Satellinet) - 2014-2015</h4>
+              </CardSubtitle>
+              <CardText>
+                <p>
+                  <ul>
+                    <li>
+                      Suivi du secteur économique des médias et de la
+                      communication en ligne
+                    </li>
+                    <li>Rédaction d'aricles d'actualité et d'analyse</li>
+                    <li>PAO et gestion de la newsletter</li>
+                  </ul>
+                </p>
+              </CardText>
+              {/* 
+              <Button>Button</Button> */}
+            </CardBody>
+          </Card>
+        </Col>
+        <Col sm="6">
+          <Card>
+            <CardBody className="text-left">
+              <CardTitle>
+                <img src={LaCroix} alt="La Croix Logo" height="100px" />
+                <h3>Journaliste Web</h3>
+              </CardTitle>
+              <CardSubtitle>
+                <h4>LaCroix.com (groupe Bayard) - 2013-2014</h4>
+              </CardSubtitle>
+              <CardText>
+                <p>
+                  <ul>
+                    <li>Rédaction d'articles d'actualité générale </li>
+                    <li>
+                      Enrichissement multimédia et data des articles print en
+                      vue de leur publication sur le site{" "}
+                    </li>
+                    <li>Frontpage edition et gestion du site en CMS</li>
+                  </ul>
+                </p>
+              </CardText>
+              {/* <Button>Button</Button> */}
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ src/Components/NavbarComponent.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import Avatar from "../Images/Avatar-AxelBizel.png"
+import "../App.css";
+
+const NavbarComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand href="/"><img src={Avatar} height="50px" /> </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/Experience">Experience</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Formation</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Stack</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">Contact</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavbarComponent;
