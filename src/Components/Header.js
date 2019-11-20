14d486fc2a5933f832a389ff3b6409315f9bd63e
Axel Bizel <axel.bizel@gmail.com>
2019-11-21 00:22:22 +0100

    test gradients et composant react strap card

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Avatar from "../Images/Avatar-AxelBizel.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
     <div className="header">
-      <img src={Avatar} />
+      <img src={Avatar} alt="Axel Bizel's Avatar" />
       <div>
         <h1>Axel Bizel</h1>
         <h2>Développeur front-end junior</h2>
+        <FontAwesomeIcon icon={faCoffee} />
       </div>
     </div>
   );
